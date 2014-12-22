27eadd4ee09b633e7ef23fb4cf6bfa272a396b50
Brian Anderson <banderson623@gmail.com>
2014-12-21 22:57:50 -0600

    basic quill editing support

--- app/assets/javascripts/setup_quill.js
+++ app/assets/javascripts/setup_quill.js
@@ -10,11 +10,11 @@
 
       quill.on('text-change', function(delta, source) {
         $field.val(quill.getHTML());
-        if (source == 'api') {
-          console.log("An API call triggered this change.");
-        } else if (source == 'user') {
-          console.log("A user action triggered this change.", quill.getText());
-        }
+        // if (source == 'api') {
+        //   console.log("An API call triggered this change.");
+        // } else if (source == 'user') {
+        //   console.log("A user action triggered this change.", quill.getText());
+        // }
       });
     }
   });
