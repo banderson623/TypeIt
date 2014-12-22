e2f566c0729b353c77f1b679bb3fb8cd9ed42f04
Brian Anderson <banderson623@gmail.com>
2014-12-21 21:34:40 -0600

    adds seed

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,7 @@
 class Document < ActiveRecord::Base
   has_many :authors
   has_many :users, :through => :authors
+  belongs_to :audio
 
   def authors_names
     users.map(&:name).join(', ')
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+brian = User.create(name: "Brian Anderson",
+                email: "brian@bitbyteyum.com")
+
+d = Document.create(name: "ATP 96",
+                    content: "")
+
+d.users << brian
+
+a = Audio.create(name: "ATP 96",
+                 url: "http://traffic.libsyn.com/atpfm/atp96.mp3")
+
+d.audio = a
+
+d.save!
